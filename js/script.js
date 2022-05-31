537e7d1c2449cbdf2625197154b4d193adf0fde1
Evgen Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-05-31 13:47:41 +0000

    Finish

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 // This file contains the JS functions for index.html
 
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-03-HTML/sw.js", {
-    scope: "/ICS2O-Unit5-03-HTML/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-04-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-04-HTML/",
   })
 }
 
@@ -15,18 +15,21 @@ function calculate() {
 
   //input
   const age = parseInt(document.getElementById('age').value)
+  const sunday = document.getElementById("sunday").checked
+  const monday = document.getElementById("monday").checked
+  const tuesday = document.getElementById("tuesday").checked
+  const wednesday = document.getElementById("wednesday").checked
+  const thursday = document.getElementById("thursday").checked
+  const friday = document.getElementById("friday").checked
+  const saturday = document.getElementById("saturday").checked
 
   // process
-  if (age >= 17) {
+  if ((tuesday == true || thursday == true) && (age > 12 && age < 21)) {
     document.getElementById("answer").innerHTML =
-      "You you are " + age + " years old, so you can see R rated movies."
-  }
-  else if (age >= 13){
-    document.getElementById("answer").innerHTML =
-      "You are " + age + " years old, so you can see PG-13 rated movies."
+      "You are eligble for student pricing."
   }
   else {
     document.getElementById("answer").innerHTML =
-      "You are " + age + " years old, so you can see G rated movies."
+      "You must pay regular price."
   }
 }
